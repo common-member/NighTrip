6be5e68c08fee7c35814fff4f1b11f85c24a7932
hisa <chre910@gmail.com>
2025-01-27 22:46:48 +0900

    add:testの設定&root_urlに変更

--- test/controllers/top_controller_test.rb
+++ test/controllers/top_controller_test.rb
@@ -2,7 +2,7 @@ require "test_helper"
 
 class TopControllerTest < ActionDispatch::IntegrationTest
   test "should get home" do
-    get top_home_url
+    get root_url
     assert_response :success
   end
 end

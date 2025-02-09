df75a9074061114b58ae688d73ec688ff10a8404
hisa <chre910@gmail.com>
2025-02-09 17:40:40 +0900

    fix:モデルバリデーションの正規表現更新

--- app/models/spot.rb
+++ app/models/spot.rb
@@ -6,7 +6,7 @@ class Spot < ApplicationRecord
 
   validates :name, presence: true
   validates :address, presence: true
-  validates :url, format: { with: /\Ahttps?:\/\/[^\s]+/i, message: "は有効なURL形式でなければなりません" }, allow_blank: true
+  validates :url, format: { with: /\Ahttps?:\/\/[^\s]+\z/i, message: "は有効なURL形式でなければなりません" }, allow_blank: true
   validates :url, presence: true, if: :url_present?
   validates :body, length: { maximum: 5000 }
   validates :image, presence: true

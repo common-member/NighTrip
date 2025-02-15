4a8c70ff0fde326527c872797b00f019b078a918
hisa <chre910@gmail.com>
2025-02-15 18:52:23 +0900

    fix:ドメイン設定を統合

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -96,13 +96,11 @@ Rails.application.configure do
   config.active_record.attributes_for_inspect = [ :id ]
 
   # Enable DNS rebinding protection and other `Host` header attacks.
-  # config.hosts = [
-  #   "example.com",     # Allow requests from example.com
-  #   /.*\.example\.com/ # Allow requests from subdomains like `www.example.com`
-  # ]
+  config.hosts = [
+    "nightrip.net",     # Allow requests from example.com
+    "www.nightrip.net"
+    # /.*\.example\.com/ # Allow requests from subdomains like `www.example.com`
+  ]
   # Skip DNS rebinding protection for the default health check endpoint.
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
-
-  config.hosts << "www.nightrip.net"
-  config.hosts << "nightrip.net"
 end

1cf2d79858877b6873c698ee355f34d90013f800
hisa <chre910@gmail.com>
2025-02-22 17:02:43 +0900

    add:メタタグの追加

--- Gemfile
+++ Gemfile
@@ -36,13 +36,22 @@ gem "bootsnap", require: false
 # Use Active Storage variants [https://guides.rubyonrails.org/active_storage_overview.html#transforming-images]
 gem "image_processing", "~> 1.2"
 
+# 独自追加群
+# デバッグ
 gem "pry"
 
+# CSS
 gem "tailwindcss-rails"
+
+# ログイン
 gem "devise"
 
+# 画像投稿周り
 gem "aws-sdk-s3", require: false
 gem "ruby-vips"
+
+# メタタグ
+gem "meta-tags"
 
 group :development, :test do
   # See https://guides.rubyonrails.org/debugging_rails_applications.html#debugging-with-the-debug-gem
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,29 @@
 module ApplicationHelper
+  def default_meta_tags
+    {
+      site: 'NighTrip',
+      title: 'NighTrip',
+      reverse: true,
+      charset: 'utf-8',
+      description: 'NighTripは、美しい夜景を投稿・共有できるアプリです。',
+      keywords: '夜景, 夜景スポット, 写真撮影, 旅行, インスタ映え, ライトアップ, 絶景, イルミネーション',
+      canonical: request.original_url,
+      separator: '|',
+      image: image_url('ogp-placeholder.png'),
+      og: {
+        site_name: :site,
+        title: title.presence || :site,
+        # description:,
+        type: 'website',
+        url: request.original_url,
+        # image:, # 動的にするために空欄
+        local: 'ja-JP',
+      },
+      twitter: {
+        site: '@hisa_dev',
+        card: 'summary_large_image',
+        # image:, # 動的にするために空欄
+      },
+    }
+  end
 end

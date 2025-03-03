c8cd307269b9df8c313469db6f2e5c6d964dda66
hisa <chre910@gmail.com>
2025-03-03 19:33:41 +0900

    add:キーワード検索が正常に動作するように

--- app/controllers/spots_controller.rb
+++ app/controllers/spots_controller.rb
@@ -4,6 +4,10 @@ class SpotsController < ApplicationController
 
   # GET /spots or /spots.json
   def index
+    # 検索条件をparamsから取り出して手動でマージ
+    search_params = params[:q] || {}
+    search_params[:name_or_address_or_body_or_user_name_cont] = params[:q][:name_or_address_or_body_or_user_name] if params.dig(:q, :name_or_address_or_body_or_user_name).present?
+
     @q = Spot.ransack(params[:q])
     @spots = @q.result.includes(:user, :prefecture).page(params[:page])
     # 都道府県の選択肢を取得

9d89b22f38ac9a17eb3a177443a1a0e3be6bb5f1
hisa <chre910@gmail.com>
2025-03-12 16:55:50 +0900

    add:マイグレーション適用

--- db/migrate/20250312073802_create_bookmarks.rb
+++ db/migrate/20250312073802_create_bookmarks.rb
@@ -1,8 +1,8 @@
 class CreateBookmarks < ActiveRecord::Migration[7.2]
   def change
     create_table :bookmarks do |t|
-      t.references :user_id, foreign_key: true
-      t.references :spot_id, foreign_key: true
+      t.references :user, foreign_key: true
+      t.references :spot, foreign_key: true
 
       t.timestamps
     end

a9e96ab22828950018b51a0f48de1c7a8cbc000e
hisa <chre910@gmail.com>
2025-04-11 21:42:52 +0900

    fix:rspecを修正

--- spec/requests/diagnoses_spec.rb
+++ spec/requests/diagnoses_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe "Diagnoses", type: :request do
   describe "GET /new" do
     it "returns http success" do
       get "/diagnosis"
-      expect(response).to have_http_status(302)
+      expect(response).to have_http_status(200)
     end
   end
 

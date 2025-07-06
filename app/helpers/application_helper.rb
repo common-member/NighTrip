53cb973ae52618322aa57b23795664d671533a4d
hisa <chre910@gmail.com>
2025-07-06 19:29:57 +0900

    add:ヘッダーのUI/UXを改善

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -33,4 +33,13 @@ module ApplicationHelper
   def spots_index_button(label = t("helpers.label.spots_index"))
     link_to label, spots_path, class: "btn btn-primary w-full hover:bg-gray-600", data: { action: "click->loading#show" }
   end
+
+  def nav_link_to(name, path, icon:, alt:, style_color: "btn btn-ghost")
+    active_class = current_page?(path) ? "btn-active" : ""
+    link_classes = [ style_color, active_class ].join(" ").strip
+
+    link_to path, class: link_classes, data: { action: "click->loading#show" } do
+      image_tag(icon, alt: alt, class: "inline w-5 h-5 mr-1 align-text-bottom") + name
+    end
+  end
 end
